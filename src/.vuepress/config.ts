ffa7c7bc51ae8be82261aa8f09262ebd02f415be
几米浆2 <2916288723@qq.com>
2023-11-14 16:43:14 +0800

    更新部署脚本

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,33 @@
 import { defineUserConfig } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/fscratch.github.io/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "FScratch's Blog",
+  description: "「FScratch」 的文档中心",
 
   theme,
 
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ]
   // Enable it with pwa
   // shouldPrefetch: false,
 });
