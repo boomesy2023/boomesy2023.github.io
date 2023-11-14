7edae0574fdc3b1633db86fe65a1e29c91a84754
几米浆2 <2916288723@qq.com>
2023-11-14 16:58:39 +0800

    更新部署脚本

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/fscratch.github.io/",
+  base: "/",
 
   lang: "zh-CN",
   title: "FScratch's Blog",
